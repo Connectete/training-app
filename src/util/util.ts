fbb87eaa7228a3bceb0de25bade99da1574043fb
You-lab <yuto.ogawa1@outlook.jp>
2023-09-07 11:26:13 +0900

    utilクラスの実装

---
+++ src/util/util.ts
@@ -0,0 +1,25 @@
+export const SanitizedDate=(dateStr:string):Date =>{
+    const date = new Date(dateStr);
+    return date;
+}
+
+export const FloatWeight=(weight:number):number =>{
+    return parseFloat(weight.toFixed(1));
+}
+ 
+
+export const MaxWeight=(weight:number):number =>{
+    if ( weight < 1000){
+        return weight;
+    } else{
+        return 0;
+    }
+}
+
+export const MissWeight=(weight:number):number =>{
+    if ( !isNaN(weight)){
+        return weight;
+    } else{
+        return 0;
+    }
+}
