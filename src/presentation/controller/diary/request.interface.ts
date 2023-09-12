77a2e2d7cbc35bd6afd2e8814f9d584af24d10ec
You-lab <yuto.ogawa1@outlook.jp>
2023-09-12 21:44:05 +0900

    正規表現の追加

--- src/presentation/controller/diary/request.interface.ts
+++ src/presentation/controller/diary/request.interface.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { IsNotEmpty, IsString, MaxLength, Matches } from 'class-validator';
 
 export class PutRequest {
@@ -12,4 +13,15 @@ export class PutRequest {
     },
   )
   contents: string;
+=======
+import { IsNotEmpty,IsString, MaxLength,Matches} from 'class-validator';
+
+export class PostRequest {
+  @IsNotEmpty({message: '日記の内容を入力して下さい'})
+  @IsString({message: '文字列を入力してください'})
+  @MaxLength(300,{message: '日記の内容は300文字までです'})
+  @Matches(/^[a-zA-Z0-9\s!@#$%^&*()_+{}\[\]:;<>,.?~\\/-\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}]+$/,
+   {message: '内容には特殊文字を除く半角英数字、一部の記号、および日本語が使用できます',})
+  contents : string;
+>>>>>>> 66df974 (正規表現の追加)
 }
