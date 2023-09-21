7b56e5cf49bd28122a7657c9ec4220af0b79763a
Yuto Ogawa <yuto.ogawa1@outlook.jp>
2023-09-21 11:23:35 +0900

    ユーザーへの返答文の文言を変更

--- src/presentation/controller/diet/request.interface.ts
+++ src/presentation/controller/diet/request.interface.ts
@@ -2,10 +2,10 @@ import { IsNotEmpty } from "class-validator";
 
 
 export class getDietRequest {
-    @IsNotEmpty({ message: "ユーザーIDを入力してください" })
+    @IsNotEmpty({ message: "データを取得出来ませんでした。再読み込みを行うか、管理者に連絡して下さい。" })
     userId!: string;
-    @IsNotEmpty({ message: "日付を入力してください" })
+    @IsNotEmpty({ message: "データを取得出来ませんでした。再読み込みを行うか、管理者に連絡して下さい。" })
     date!: Date;
-    @IsNotEmpty({ message: "タイプを入力してください" })
+    @IsNotEmpty({ message: "データを取得出来ませんでした。再読み込みを行うか、管理者に連絡して下さい。" })
     type!: string;
 }
