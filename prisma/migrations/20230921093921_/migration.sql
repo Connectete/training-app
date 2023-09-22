ccf8a44da2b4a3f5f8e87c8e6558e609ad91d932
Yuto Ogawa <yuto.ogawa1@outlook.jp>
2023-09-22 10:37:39 +0900

    migration.sqlの統一

--- prisma/migrations/20230921093921_/migration.sql
+++ prisma/migrations/20230921093921_/migration.sql
@@ -25,7 +25,6 @@ CREATE TABLE "Accounts" (
 CREATE TABLE "Exercises" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "calorie" INTEGER NOT NULL,
 
     CONSTRAINT "Exercises_pkey" PRIMARY KEY ("id")
 );
@@ -51,14 +50,15 @@ CREATE TABLE "BodyRecords" (
 );
 
 -- CreateTable
-CREATE TABLE "ExirciseRecords" (
+CREATE TABLE "ExerciseRecords" (
     "id" SERIAL NOT NULL,
     "user_id" TEXT NOT NULL,
     "exercise_id" INTEGER NOT NULL,
-    "date_time" TIMESTAMP(3) NOT NULL,
-    "time" INTEGER NOT NULL,
+    "date" DATE NOT NULL,
+    "timeCount" INTEGER NOT NULL DEFAULT 0,
+    "calorie" INTEGER NOT NULL DEFAULT 0,
 
-    CONSTRAINT "ExirciseRecords_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ExerciseRecords_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -80,7 +80,7 @@ CREATE TABLE "Diets" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Diets_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Diets_pkey" PRIMARY KEY ("user_id")
 );
 
 -- CreateTable
@@ -116,7 +116,13 @@ CREATE UNIQUE INDEX "Exercises_name_key" ON "Exercises"("name");
 CREATE UNIQUE INDEX "BodyRecords_user_id_date_key" ON "BodyRecords"("user_id", "date");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "ExerciseRecords_user_id_date_exercise_id_key" ON "ExerciseRecords"("user_id", "date", "exercise_id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Diaries_user_id_date_key" ON "Diaries"("user_id", "date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Diets_user_id_date_time_type_key" ON "Diets"("user_id", "date_time", "type");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_ColumnToColumnTag_AB_unique" ON "_ColumnToColumnTag"("A", "B");
@@ -134,10 +140,10 @@ ALTER TABLE "Goals" ADD CONSTRAINT "Goals_user_id_fkey" FOREIGN KEY ("user_id") 
 ALTER TABLE "BodyRecords" ADD CONSTRAINT "BodyRecords_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ExirciseRecords" ADD CONSTRAINT "ExirciseRecords_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ExerciseRecords" ADD CONSTRAINT "ExerciseRecords_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ExirciseRecords" ADD CONSTRAINT "ExirciseRecords_exercise_id_fkey" FOREIGN KEY ("exercise_id") REFERENCES "Exercises"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ExerciseRecords" ADD CONSTRAINT "ExerciseRecords_exercise_id_fkey" FOREIGN KEY ("exercise_id") REFERENCES "Exercises"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Diaries" ADD CONSTRAINT "Diaries_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
