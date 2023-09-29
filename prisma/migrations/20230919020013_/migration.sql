a1bef342a0df86f141e236ff7afda237638bf76d
Yuto Ogawa <yuto.ogawa1@outlook.jp>
2023-09-29 15:10:42 +0900

    seeds.tsとmigrationの修正

--- prisma/migrations/20230919020013_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The primary key for the `Goals` table will be changed. If it partially fails, the table could be left without primary key constraint.
-
-*/
--- AlterTable
-ALTER TABLE "Goals" DROP CONSTRAINT "Goals_pkey",
-ADD CONSTRAINT "Goals_pkey" PRIMARY KEY ("user_id");
